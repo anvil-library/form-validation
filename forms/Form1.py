3824b30cf29fe88283ca8a0348e6d37d4c8d8c4c
demos@anvil.works <demos@anvil.works>
2018-11-27 17:21:49 +0000

    Saved automatically

---
+++ forms/Form1.py
@@ -0,0 +1,9 @@
+from anvil import *
+
+class Form1(Form1Template):
+
+  def __init__(self, **properties):
+    # You must call self.init_components() before doing anything else in this function
+    self.init_components(**properties)
+
+    # Any code you write here will run when the form opens.
